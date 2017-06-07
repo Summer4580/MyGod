a926efd0d9b49519cdc207d31fb2085d4acca633
Summer4580 <20161104580X@Mac01.local>
2017-06-07 10:43:23 +0800

    冒泡
    输入n个数之后从小到大排序

--- MyGod/main.c
+++ MyGod/main.c
@@ -8,8 +8,30 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[])
+{
     // insert code here...
-    printf("Hello, World!\n");
+    int i,j,temp,n;
+    int a[10001];
+    while(scanf("%d",&n)!=EOF)
+    {
+        for(i=0;i<n;i++)
+        {
+            scanf("%d",&a[i]);
+        }
+        for(i=0;i<n;i++)
+            for(j=0;j<n-i-1;j++)
+            {
+                if(a[j]>a[j+1])
+                {
+                    temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+            }
+        for(i=0;i<n;i++)
+            printf("%d\t",a[i]);
+        printf("\n");
+    }
     return 0;
 }
