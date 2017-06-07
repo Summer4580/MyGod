ecef71ddd7b1002b2088107b4084fc8fab2dd2c1
Summer4580 <20161104580X@mac01.local>
2017-06-07 10:22:29 +0800

    Initial Commit

---
+++ MyGod/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MyGod
+//
+//  Created by Summer on 2017/6/7.
+//  Copyright © 2017年 Summer. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
